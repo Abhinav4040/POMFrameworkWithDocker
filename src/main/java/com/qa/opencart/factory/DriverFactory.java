1881ab5b81dff4435ef5ee78926deaf37c6cd20a
Abhinav Jangid <abhinavjangid@192.168.1.5>
2022-11-02 23:43:58 +0530

    Added romote execution setup with docker and cross browser logic

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -13,6 +15,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -41,19 +44,27 @@ public class DriverFactory {
 		optionsManager = new OptionsManager(prop);
 
 		if (browserName.equals("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			// driver = new ChromeDriver();
-			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				remoteDriver("chrome");
+			} else {
+				WebDriverManager.chromedriver().setup();
+				tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+			}
+
 		} else if (browserName.equals("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			// driver = new FirefoxDriver();
-			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				remoteDriver("firefox");
+			} else {
+				WebDriverManager.firefoxdriver().setup();
+				tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+			}
+
 		} else if (browserName.equals("edge")) {
 			WebDriverManager.edgedriver().setup();
-			// driver = new EdgeDriver();
 			tlDriver.set(new EdgeDriver());
 		} else if (browserName.equals("safari")) {
-			// driver = new SafariDriver();
 			tlDriver.set(new SafariDriver());
 		} else {
 			System.out.println("Please pass the right browser name: " + browserName);
@@ -65,6 +76,34 @@ public class DriverFactory {
 
 		return getDriver();
 
+	}
+
+	private void remoteDriver(String browser) {
+		
+		
+		System.out.println("Running the Tc's on Remote server");
+
+		if (browser.equals("chrome")) {
+
+			try {
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("HubUrl")), optionsManager.getChromeOptions()));
+
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		else if (browser.equals("firefox")) {
+
+			try {
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("HubUrl")), optionsManager.getFirefoxOptions()));
+
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static synchronized WebDriver getDriver() {
@@ -83,7 +122,7 @@ public class DriverFactory {
 		// mvn clean install -Denv="dev"
 		// mvn clean install
 
-		//String envName = System.getenv("env");// stage/uat/qa/dev
+		// String envName = System.getenv("env");// stage/uat/qa/dev
 		String envName = System.getProperty("env");
 		System.out.println("-----> Running test cases on environment: ----->" + envName);
 
@@ -134,37 +173,24 @@ public class DriverFactory {
 
 		return prop;
 	}
-	
-	
+
 	/**
 	 * take screenshot
 	 */
 	public static String getScreenshot() {
-		
+
 		File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-		
-		
+
 		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
 		File destination = new File(path);
-		
+
 		try {
 			FileUtils.copyFile(srcFile, destination);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return path;
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
